65766a1c997a07d85309a8feefeb38454c2d8d28
Harshit Mahajan <hmahajan@atlassian.com>
2022-05-25 10:48:34 +0530

    globalState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import { JSXElementConstructor, Key, ReactElement, useEffect, useState } from "react";
+import {
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  useEffect,
+  useState,
+  createContext,
+  useReducer,
+  ReactChild,
+  useContext
+} from "react";
 
 // react-router components
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
@@ -24,6 +34,43 @@ import { setMiniSidenav, setOpenConfigurator, useMaterialUIController } from "co
 // Images
 import brandWhite from "assets/images/logo-ct.png";
 import brandDark from "assets/images/logo-ct-dark.png";
+
+const defaultGlobalState = {
+  num: 0,
+  text: "foo",
+  bool: false
+};
+const globalStateContext = createContext(defaultGlobalState);
+const dispatchStateContext = createContext(undefined);
+
+// @ts-ignore
+const GlobalStateProvider = ({children}) => {
+  const [state, dispatch] = useReducer(
+    (state:any , newValue: any) => ({ ...state, ...newValue }),
+    defaultGlobalState
+  );
+  return (
+    <globalStateContext.Provider value={state}>
+      <dispatchStateContext.Provider value={dispatch}>
+        {children}
+      </dispatchStateContext.Provider>
+    </globalStateContext.Provider>
+  );
+};
+
+const useGlobalState = () => [
+  useContext(globalStateContext),
+  useContext(dispatchStateContext)
+];
+
+const Counter = () => {
+  const [state, dispatch] = useGlobalState();
+  return (
+    <button onClick={() => dispatch({ num: state.num + 1 })}>
+      {state.num}
+    </button>
+  );
+};
 
 export default function App() {
   const [controller, dispatch] = useMaterialUIController();
@@ -115,27 +162,30 @@ export default function App() {
   );
 
   return (
-    <ThemeProvider theme={darkMode ? themeDark : theme}>
-      <CssBaseline />
-      {layout === "dashboard" && (
-        <>
-          <Sidenav
-            color={sidenavColor}
-            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Localstack UI"
-            routes={routes}
-            onMouseEnter={handleOnMouseEnter}
-            onMouseLeave={handleOnMouseLeave}
-          />
-          <Configurator />
-          {configsButton}
-        </>
-      )}
-      {layout === "vr" && <Configurator />}
-      <Routes>
-        {getRoutes(routes)}
-        <Route path="*" element={<Navigate to="/dashboards/analytics" />} />
-      </Routes>
-    </ThemeProvider>
+    <GlobalStateProvider>
+      <Counter/>
+      <ThemeProvider theme={darkMode ? themeDark : theme}>
+        <CssBaseline />
+        {layout === "dashboard" && (
+          <>
+            <Sidenav
+              color={sidenavColor}
+              brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+              brandName="Localstack UI"
+              routes={routes}
+              onMouseEnter={handleOnMouseEnter}
+              onMouseLeave={handleOnMouseLeave}
+            />
+            <Configurator />
+            {configsButton}
+          </>
+        )}
+        {layout === "vr" && <Configurator />}
+        <Routes>
+          {getRoutes(routes)}
+          <Route path="*" element={<Navigate to="/dashboards/analytics" />} />
+        </Routes>
+      </ThemeProvider>
+    </GlobalStateProvider>
   );
 }
