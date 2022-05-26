b87ed23751c45059b68828a86dbeb55dfe911163
Utkarsh Srivastava <usrivastava92@gmail.com>
2022-05-26 11:35:05 +0530

    fixed electron integration

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,3 @@
-// ./public/electron.js
 const path = require('path');
 
 const { app, BrowserWindow } = require('electron');
@@ -13,28 +12,15 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
-  // Open the DevTools.
   if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
+    win.loadURL('http://localhost:3000').then(r => {});
+  } else {
+    win.loadFile(path.join(__dirname, "../build/index.html")).then(r => {})
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bars to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
