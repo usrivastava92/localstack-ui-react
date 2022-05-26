b07816c746a66670bcb5a6feb661dd108a917f50
utkarsh <usrivastava92@gmail.com>
2022-05-26 13:50:54 +0530

    pulled origin

--- src/layouts/pages/aws/ElasticSearchDashboard/index.tsx
+++ src/layouts/pages/aws/ElasticSearchDashboard/index.tsx
@@ -1,30 +1,29 @@
 // @mui material components
 // Settings page components
 
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import AwsDashboardLayout from "../AwsDashboardLayout";
-import {Card} from "@mui/material";
-import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
-import {AWSProfile, nullAwsProfile} from "../types/awsTypes";
-import {AWSProfileContext} from "../../../../context";
-import {getClientConfig} from "../utils/awsUtils";
+import { Card } from "@mui/material";
+import MDBox from "@/components/MDBox";
+import MDTypography from "@/components/MDTypography";
+import { AWSProfile, nullAwsProfile } from "../types/awsTypes";
+import { AWSProfileContext } from "@/context";
+import { getClientConfig } from "../utils/awsUtils";
 import {
   DescribeElasticsearchDomainCommand,
   ElasticsearchServiceClient,
   ListDomainNamesCommand
 } from "@aws-sdk/client-elasticsearch-service";
-import {ColumnDefinition, TableData} from "../types/tableTypes";
+import { ColumnDefinition, TableData } from "../types/tableTypes";
 import DefaultCell from "../../../ecommerce/orders/order-list/components/DefaultCell";
 import Autocomplete from "@mui/material/Autocomplete";
-import MDInput from "../../../../components/MDInput";
-import DataTable from "../../../../examples/Tables/DataTable";
-import {Client} from "@elastic/elasticsearch";
-import {CatIndicesIndicesRecord, CatIndicesResponse} from "@elastic/elasticsearch/lib/api/types";
+import MDInput from "@/components/MDInput";
+import { Client } from "@elastic/elasticsearch";
+import { CatIndicesIndicesRecord, CatIndicesResponse } from "@elastic/elasticsearch/lib/api/types";
 
 const columnDefinitions: ColumnDefinition[] = [
-  {Header: 'Index Name', accessor: "name", Cell: ({value}: { value: string }) => <DefaultCell value={value}/>}
-]
+  { Header: "Index Name", accessor: "name", Cell: ({ value }: { value: string }) => <DefaultCell value={value} /> }
+];
 
 interface RowDefinition {
   name: string,
@@ -43,14 +42,14 @@ function getRows(indices: CatIndicesIndicesRecord[]): RowDefinition[] {
       name: index.index,
       docsCount: index.docsCount,
       bulkAvgSizeInBytes: index.bulkAvgSizeInBytes,
-      status: index.status,
-    }
+      status: index.status
+    };
   });
 }
 
 function getTableData(response: CatIndicesResponse): TableData {
   if (!response) {
-    return {columns: [], rows: []};
+    return { columns: [], rows: [] };
   }
   return {
     columns: columnDefinitions,
@@ -73,34 +72,34 @@ function Content(): JSX.Element {
       client.send(new ListDomainNamesCommand({}))
         .then(output => {
           if (output && output.DomainNames && output.DomainNames.length > 0) {
-            setDomains(output.DomainNames.map<string>(domainInfo => domainInfo.DomainName))
+            setDomains(output.DomainNames.map<string>(domainInfo => domainInfo.DomainName));
           }
         }).catch(error => console.error(error));
     }
   }, []);
 
   function listIndexes(domainName: string) {
-    client.send(new DescribeElasticsearchDomainCommand({DomainName: domainName}))
+    client.send(new DescribeElasticsearchDomainCommand({ DomainName: domainName }))
       .then(output => {
         if (output && output.DomainStatus && output.DomainStatus.Endpoint) {
           if (domainName !== selectedDomain) {
             setSelectedDomain(domainName);
             setEsClient(new Client({
               node: output.DomainStatus.Endpoint
-            }))
+            }));
           }
           esClient.cat.indices()
             .then(output => setTableData(getTableData(output)))
-            .catch(error => console.error(error))
+            .catch(error => console.error(error));
         }
       })
-      .catch(error => console.error(error))
-    console.log("scanning index")
+      .catch(error => console.error(error));
+    console.log("scanning index");
   }
 
   return (
     <div>
-      <MDBox my={3} sx={{maxHeight: "50%"}}>
+      <MDBox my={3} sx={{ maxHeight: "50%" }}>
         <Card>
           <MDBox p={3} lineHeight={1} display="flex" justifyContent="space-between">
             <MDBox>
@@ -124,14 +123,13 @@ function Content(): JSX.Element {
             </MDBox>
             <Autocomplete
               disableClearable
-              sx={{width: "12rem", borderRadius: 3}}
+              sx={{ width: "12rem", borderRadius: 3 }}
               value={selectedDomain ? selectedDomain : "No Domain Selected"}
               options={domains}
               onChange={(e, v) => listIndexes(v as string)}
-              renderInput={(params) => <MDInput {...params} label="Domain" fullWidth/>}
+              renderInput={(params) => <MDInput {...params} label="Domain" fullWidth />}
             />
           </MDBox>
-          <DataTable table={tableData} canSearch={true} stickyHeader={true}/>
         </Card>
       </MDBox>
     </div>
@@ -141,7 +139,7 @@ function Content(): JSX.Element {
 function ElasticSearchDashboard(): JSX.Element {
   return (
     <AwsDashboardLayout title="Elastic Search" subTitle="Create/Choose a profile">
-      <Content/>
+      <Content />
     </AwsDashboardLayout>
   );
 }
